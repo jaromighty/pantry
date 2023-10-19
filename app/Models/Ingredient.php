0b096264ad374869c87bf92d1e9a67224974f3ba
Jarom Rial <jarom.rial@proton.me>
2023-10-19 05:39:58 -0600

    add relationship between recipes and ingredients

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ingredient extends Model
 {
@@ -12,4 +13,9 @@ class Ingredient extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function recipes(): BelongsToMany
+    {
+        return $this->belongsToMany(Recipe::class);
+    }
 }
--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Recipe extends Model
 {
@@ -12,4 +13,9 @@ class Recipe extends Model
     protected $fillable = [
         'name',
     ];
+
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
 }
