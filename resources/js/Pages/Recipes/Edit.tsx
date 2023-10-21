19b0f3828ae713401ff733f6c4470b5490dabb45
Jarom Rial <jarom.rial@proton.me>
2023-10-21 08:22:45 -0600

    add type to recipe edit page

--- resources/js/Pages/Recipes/Edit.tsx
+++ resources/js/Pages/Recipes/Edit.tsx
@@ -1,7 +1,23 @@
 import {Head, useForm} from "@inertiajs/react";
 import Authenticated from "@/Layouts/AuthenticatedLayout";
 import {PageProps, Recipe} from "@/types";
-import {useState} from "react";
+import {Fragment, useState} from "react";
+import {Listbox, Transition} from "@headlessui/react";
+import {CheckIcon, ChevronUpDownIcon} from "@heroicons/react/24/solid";
+import {classNames} from "@/Utils/classNames";
+
+interface RecipeType {
+  id: number
+  name: string
+  value: string
+}
+const types: RecipeType[] = [
+  { id: 1, name: 'Breakfast', value: 'breakfast' },
+  { id: 1, name: 'Lunch', value: 'lunch' },
+  { id: 1, name: 'Snack', value: 'snack' },
+  { id: 1, name: 'Dinner', value: 'dinner' },
+  { id: 1, name: 'Dessert', value: 'dessert' },
+];
 
 export default function RecipeEdit ({ auth, recipe }: PageProps<{ recipe: Recipe }>) {
   const [newIngredient, setNewIngredient] = useState('');
@@ -9,6 +25,7 @@ export default function RecipeEdit ({ auth, recipe }: PageProps<{ recipe: Recipe
     name: recipe.name,
     ingredients: recipe.ingredients.map(i => ({
       idx: i.id,
+      type: types.find(type => type.value === recipe.type),
       name: i.name,
     })),
   });
@@ -65,6 +82,66 @@ export default function RecipeEdit ({ auth, recipe }: PageProps<{ recipe: Recipe
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
+                </div>
+
+                <div className="col-span-full">
+                  <Listbox value={data.type} onChange={(value) => setData('type', value)}>
+                    {({ open }) => (
+                      <>
+                        <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">Recipe type</Listbox.Label>
+                        <div className="relative mt-2">
+                          <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
+                            <span className="block truncate">{data.type ? data.type.name : 'Select a recipe type'}</span>
+                            <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                              <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+                            </span>
+                          </Listbox.Button>
+
+                          <Transition
+                            show={open}
+                            as={Fragment}
+                            leave="transition ease-in duration-100"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                          >
+                            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                              {types.map((type) => (
+                                <Listbox.Option
+                                  key={type.id}
+                                  className={({ active }) =>
+                                    classNames(
+                                      active ? 'bg-indigo-600 text-white' : 'text-gray-900',
+                                      'relative cursor-default select-none py-2 pl-3 pr-9'
+                                    )
+                                  }
+                                  value={type}
+                                >
+                                  {({ selected, active }) => (
+                                    <>
+                                      <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
+                                        {type.name}
+                                      </span>
+
+                                      {selected ? (
+                                        <span
+                                          className={classNames(
+                                            active ? 'text-white' : 'text-indigo-600',
+                                            'absolute inset-y-0 right-0 flex items-center pr-4'
+                                          )}
+                                        >
+                                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                                        </span>
+                                      ) : null}
+                                    </>
+                                  )}
+                                </Listbox.Option>
+                              ))}
+                            </Listbox.Options>
+                          </Transition>
+                        </div>
+                      </>
+                    )}
+                  </Listbox>
                 </div>
 
                 <div className="col-span-full">
