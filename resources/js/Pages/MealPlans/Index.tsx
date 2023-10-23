ba95c5d04862ac8ff50d3ad99aa857ab7f8b84b9
Jarom Rial <jarom.rial@proton.me>
2023-10-22 19:18:39 -0600

    only display meal plan list when more than 0

--- resources/js/Pages/MealPlans/Index.tsx
+++ resources/js/Pages/MealPlans/Index.tsx
@@ -24,55 +24,57 @@ export default function MealPlanIndex ({ auth, mealPlans }: PageProps<{ mealPlan
           </div>
         </div>
 
-        <ul
-          role="list"
-          className="mt-4 divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
-        >
-          {mealPlans.map((mealPlan) => (
-            <li key={mealPlan.id} className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
-              <div className="flex min-w-0 gap-x-4">
-                {/*<img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={mealPlan.imageUrl} alt="" />*/}
-                <div className="min-w-0 flex-auto">
-                  <p className="text-sm font-semibold leading-6 text-gray-900">
-                    <Link href={route('meal-plans.edit', [mealPlan])} className="flex gap-x-1">
-                      <span className="absolute inset-x-0 -top-px bottom-0" />
-                      <time dateTime={dayjs(mealPlan.start_date).format('YYYY-MM')}>
-                        {dayjs(mealPlan.start_date).format('MMM D, YYYY')}
-                      </time>
-                      to
-                      <time dateTime={dayjs(mealPlan.end_date).format('YYYY-MM')}>
-                        {dayjs(mealPlan.end_date).format('MMM D, YYYY')}
-                      </time>
-                    </Link>
-                  </p>
-                  {/*<p className="mt-1 flex text-xs leading-5 text-gray-500">*/}
-                  {/*  <a href={`mailto:${mealPlan.email}`} className="relative truncate hover:underline">*/}
-                  {/*    {mealPlan.email}*/}
-                  {/*  </a>*/}
-                  {/*</p>*/}
+        {mealPlans.length > 0 && (
+          <ul
+            role="list"
+            className="mt-4 divide-y divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+          >
+            {mealPlans.map((mealPlan) => (
+              <li key={mealPlan.id} className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6">
+                <div className="flex min-w-0 gap-x-4">
+                  {/*<img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={mealPlan.imageUrl} alt="" />*/}
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">
+                      <Link href={route('meal-plans.edit', [mealPlan])} className="flex gap-x-1">
+                        <span className="absolute inset-x-0 -top-px bottom-0" />
+                        <time dateTime={dayjs(mealPlan.start_date).format('YYYY-MM')}>
+                          {dayjs(mealPlan.start_date).format('MMM D, YYYY')}
+                        </time>
+                        to
+                        <time dateTime={dayjs(mealPlan.end_date).format('YYYY-MM')}>
+                          {dayjs(mealPlan.end_date).format('MMM D, YYYY')}
+                        </time>
+                      </Link>
+                    </p>
+                    {/*<p className="mt-1 flex text-xs leading-5 text-gray-500">*/}
+                    {/*  <a href={`mailto:${mealPlan.email}`} className="relative truncate hover:underline">*/}
+                    {/*    {mealPlan.email}*/}
+                    {/*  </a>*/}
+                    {/*</p>*/}
+                  </div>
                 </div>
-              </div>
-              <div className="flex shrink-0 items-center gap-x-4">
-                {/*<div className="hidden sm:flex sm:flex-col sm:items-end">*/}
-                {/*  <p className="text-sm leading-6 text-gray-900">{mealPlan.role}</p>*/}
-                {/*  {mealPlan.lastSeen ? (*/}
-                {/*    <p className="mt-1 text-xs leading-5 text-gray-500">*/}
-                {/*      Last seen <time dateTime={mealPlan.lastSeenDateTime}>{mealPlan.lastSeen}</time>*/}
-                {/*    </p>*/}
-                {/*  ) : (*/}
-                {/*    <div className="mt-1 flex items-center gap-x-1.5">*/}
-                {/*      <div className="flex-none rounded-full bg-emerald-500/20 p-1">*/}
-                {/*        <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />*/}
-                {/*      </div>*/}
-                {/*      <p className="text-xs leading-5 text-gray-500">Online</p>*/}
-                {/*    </div>*/}
-                {/*  )}*/}
-                {/*</div>*/}
-                <ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
-              </div>
-            </li>
-          ))}
-        </ul>
+                <div className="flex shrink-0 items-center gap-x-4">
+                  {/*<div className="hidden sm:flex sm:flex-col sm:items-end">*/}
+                  {/*  <p className="text-sm leading-6 text-gray-900">{mealPlan.role}</p>*/}
+                  {/*  {mealPlan.lastSeen ? (*/}
+                  {/*    <p className="mt-1 text-xs leading-5 text-gray-500">*/}
+                  {/*      Last seen <time dateTime={mealPlan.lastSeenDateTime}>{mealPlan.lastSeen}</time>*/}
+                  {/*    </p>*/}
+                  {/*  ) : (*/}
+                  {/*    <div className="mt-1 flex items-center gap-x-1.5">*/}
+                  {/*      <div className="flex-none rounded-full bg-emerald-500/20 p-1">*/}
+                  {/*        <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />*/}
+                  {/*      </div>*/}
+                  {/*      <p className="text-xs leading-5 text-gray-500">Online</p>*/}
+                  {/*    </div>*/}
+                  {/*  )}*/}
+                  {/*</div>*/}
+                  <ChevronRightIcon className="h-5 w-5 flex-none text-gray-400" aria-hidden="true" />
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </Authenticated>
   </>;
