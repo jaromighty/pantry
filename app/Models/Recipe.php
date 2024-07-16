fbb18b7739ad99786bff5007c8e1b41386173af0
Jarom Rial <jarom@firebirdtech.dev>
2024-07-16 14:27:56 -0600

    add image_url attribute and updateImage method

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
 
 class Recipe extends Model
 {
@@ -16,6 +18,10 @@ class Recipe extends Model
         'image_path',
     ];
 
+    protected $appends = [
+        'image_url',
+    ];
+
     public function ingredients(): BelongsToMany
     {
         return $this->belongsToMany(Ingredient::class);
@@ -24,5 +30,25 @@ class Recipe extends Model
     public function meals(): BelongsToMany
     {
         return $this->belongsToMany(Meal::class);
+    }
+
+    public function updateImage(UploadedFile $image): void
+    {
+        tap($this->image_path, function ($previous) use ($image) {
+            $this->forceFill([
+                'image_path' => $image->storePublicly('images', 'public'),
+            ])->save();
+
+            if ($previous) {
+                Storage::disk('public')->delete($previous);
+            }
+        });
+    }
+
+    public function getImageUrlAttribute(): string|null
+    {
+        return $this->image_path
+            ? Storage::disk('public')->url($this->image_path)
+            : null;
     }
 }
