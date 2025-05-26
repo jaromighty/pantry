803d02e3eb91e0483bd6bd90a4baf2970a5c8726
Jarom Rial <jarom@firebirdtech.dev>
2025-05-25 20:53:50 -0600

    update app settings model

--- app/Models/AppSetting.php
+++ app/Models/AppSetting.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class AppSetting extends Model
 {
@@ -12,4 +13,14 @@ class AppSetting extends Model
     protected $fillable = [
         'active_program_id',
     ];
+
+    public function activeProgram(): BelongsTo
+    {
+        return $this->belongsTo(Program::class, 'active_program_id');
+    }
+
+    public static function getActiveProgram(): ?Program
+    {
+        return static::first()?->activeProgram;
+    }
 }
